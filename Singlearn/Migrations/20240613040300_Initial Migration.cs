bbe9178b735277baf301dc8d42989ba9b7583f53
Wigsicle <Wigsicle@gmail.com>
2024-07-06 03:43:30 +0800

    Created DB tables

--- Singlearn/Migrations/20240613040300_Initial Migration.cs
+++ Singlearn/Migrations/20240613040300_Initial Migration.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -11,82 +12,295 @@ namespace SinglearnWeb.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Staffs",
-                columns: table => new
-                {
-                    staffId = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    userId = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    contactNo = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Staffs", x => x.staffId);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Students",
-                columns: table => new
-                {
-                    studentId = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    userId = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    contactNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    classId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Students", x => x.studentId);
-                });
 
             migrationBuilder.CreateTable(
                 name: "Users",
                 columns: table => new
                 {
-                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    user_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     email = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     password = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     role = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Users", x => x.id);
+                    table.PrimaryKey("PK_Users", x => x.user_id);
                 });
 
             migrationBuilder.CreateTable(
-                name: "Class",
+                name: "Staff",
                 columns: table => new
                 {
-                    classId = table.Column<int>(type: "int", nullable: false),
-                    className = table.Column<string>(type: "nvarchar(max)", nullable:false),
-                    teacherId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                    staff_id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    user_id = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    contact_no = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Class", x => x.classId);
-                    table.ForeignKey(
-                        name: "FK_Class_teacherId",
-                        column: x => x.teacherId,
-                        principalTable: "Staffs",
-                        principalColumn: "staffId",
-                        onDelete: ReferentialAction.Cascade);
+                    table.PrimaryKey("PK_Staff", x => x.staff_id);
                 });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    student_id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    user_id = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    contact_no = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    class_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.student_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    class_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable:false),
+                    teacher_id = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.class_id);
+                    table.ForeignKey(
+                        name: "FK_Classes_teacher_id",
+                        column: x => x.teacher_id,
+                        principalTable: "Staff",
+                        principalColumn: "staff_id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    subject_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<String>(type: "nvarchar(max)", nullable: false),
+                    year = table.Column<int>(type: "int", nullable: false),
+                    academic_level = table.Column<String>(type: "nvarchar(max)", nullable: false),
+                    no_chapters = table.Column<int>(type: "int", nullable:false),
+                    image = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.subject_id);
+                }
+                );
+
+            migrationBuilder.CreateTable(
+                name: "SubjectTeacherClasses",
+                columns: table => new
+                {
+                    stc_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    subject_id = table.Column<int>(type: "int", nullable: false),
+                    teacher_id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    class_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectTeacherClasses", x => x.stc_id);
+                    table.ForeignKey(
+                        name: "FK_SubjectTeacherClasses_subject_id",
+                        column: x => x.subject_id,
+                        principalTable: "Subjects",
+                        principalColumn: "subject_id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_SubjectTeacherClasses_teacher_id",
+                        column: x => x.teacher_id,
+                        principalTable: "Staff",
+                        principalColumn: "staff_id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_SubjectTeacherClasses_class_id",
+                        column: x => x.class_id,
+                        principalTable: "Classes",
+                        principalColumn: "class_id",
+                        onDelete: ReferentialAction.NoAction);
+                }
+                );
+
+            migrationBuilder.CreateTable(
+                name: "Announcements",
+                columns: table => new
+                {
+                    announcement_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    subject_id = table.Column<int>(type: "int", nullable: false),
+                    teacher_id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    class_id = table.Column<int>(type: "int", nullable: false),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    date = table.Column<DateTime>(type: "DateTime", nullable : false),
+                    message_body = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Announcements", x => x.announcement_id);
+                    table.ForeignKey(
+                        name: "FK_Announcements_subject_id",
+                        column: x => x.subject_id,
+                        principalTable: "Subjects",
+                        principalColumn: "subject_id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Announcements_teacher_id",
+                        column: x => x.teacher_id,
+                        principalTable: "Staff",
+                        principalColumn: "staff_id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Announcements_class_id",
+                        column: x => x.class_id,
+                        principalTable: "Classes",
+                        principalColumn: "class_id",
+                        onDelete: ReferentialAction.NoAction);
+                }
+                );
+
+            migrationBuilder.CreateTable(
+                name: "Materials",
+                columns: table => new
+                {
+                    material_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    subject_id = table.Column<int>(type: "int", nullable: false),
+                    teacher_id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    class_id = table.Column<int>(type: "int", nullable: false),
+                    name = table.Column<int>(type: "int", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    chapter_id = table.Column<int>(type: "int", nullable: false),
+                    type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    link = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materials", x => x.material_id);
+                    table.ForeignKey(
+                        name: "FK_Materials_subject_id",
+                        column: x => x.subject_id,
+                        principalTable: "Subjects",
+                        principalColumn: "subject_id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Materials_teacher_id",
+                        column: x => x.teacher_id,
+                        principalTable: "Staff",
+                        principalColumn: "staff_id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Materials_class_id",
+                        column: x => x.class_id,
+                        principalTable: "Classes",
+                        principalColumn: "class_id",
+                        onDelete: ReferentialAction.NoAction);
+                }
+                );
+
+            migrationBuilder.CreateTable(
+                name: "ChapterNames",
+                columns: table => new
+                {
+                    chapter_name_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    chapter_id = table.Column<int>(type: "int", nullable: false),
+                    subject_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChapterNames", x => x.chapter_name_id);
+                    table.ForeignKey(
+                        name: "FK_ChapterName_subject_id",
+                        column: x => x.subject_id,
+                        principalTable: "Subjects",
+                        principalColumn: "subject_id",
+                        onDelete: ReferentialAction.NoAction);
+                }
+                );
+
+            migrationBuilder.CreateTable(
+                name: "Templates",
+                columns: table => new
+                {
+                    template_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    layout = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Templates", x => x.template_id);
+                }
+                );
+
+            migrationBuilder.CreateTable(
+                name: "STCTemplate",
+                columns: table => new
+                {
+                    stc_t_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    stc_id = table.Column<int>(type: "int", nullable: false),
+                    template_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_STCTemplate", x => x.stc_t_id);
+                    table.ForeignKey(
+                        name: "FK_STCTemplate_template_id",
+                        column: x => x.template_id,
+                        principalTable: "Templates",
+                        principalColumn: "template_id",
+                        onDelete: ReferentialAction.NoAction);
+                }
+                );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Staffs");
+                name: "Staff");
 
             migrationBuilder.DropTable(
                 name: "Students");
 
             migrationBuilder.DropTable(
                 name: "Users");
+
             migrationBuilder.DropTable(
-                name: "Class");
+                name: "Classes");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "SubjectTeacherClasses");
+
+            migrationBuilder.DropTable(
+                name: "Announcements");
+
+            migrationBuilder.DropTable(
+                name: "Materials");
+
+            migrationBuilder.DropTable(
+                name: "ChapterNames");
+
+            migrationBuilder.DropTable(
+                name: "Templates");
+
+            migrationBuilder.DropTable(
+                name: "STCTemplate");
+
         }
     }
 }
