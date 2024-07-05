5e9456b613447f2ddc078a610241c0d66a0bfac6
wigsicle <wigsicle@gmail.com>
2024-07-05 17:14:01 +0800

    Add project files.

---
+++ Singlearn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Singlearn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
