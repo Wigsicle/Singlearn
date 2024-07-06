1bd54d84cf8c0972f8b9343d827a3d9468d0b51e
wigsicle <wigsicle@gmail.com>
2024-07-06 16:27:36 +0800

    chapternames model

--- Singlearn/Models/Entities/ChapterName.cs
+++ Singlearn/Models/Entities/ChapterName.cs
@@ -1,6 +1,16 @@
-﻿namespace Singlearn.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Singlearn.Models.Entities
 {
     public class ChapterName
     {
+        [Key]
+        public int chapter_name_id { get; set; }
+
+        public string name { get; set; }
+
+        public int chapter_id { get; set; }
+
+        public int subject_id { get; set; }
     }
 }
