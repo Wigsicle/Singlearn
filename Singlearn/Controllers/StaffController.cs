0cea5e9f47befbf4f857bb99ddab5b3ff8cc659f
FARM YONG WEI <2300874@sit.singaporetech.edu.sg>
2024-07-06 14:05:18 +0800

    added homework hub

---
+++ Singlearn/Controllers/StaffController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SinglearnWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SinglearnWeb.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public StaffController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        public IActionResult home()
+        {
+            return View();
+        }
+
+        public IActionResult profile()
+        {
+            return View();
+        }
+
+        public IActionResult homeworkhub_subject()
+        {
+            return View();
+        }
+
+        public IActionResult homeworkhub_class()
+        {
+            return View();
+        }
+
+        public IActionResult homeworkhub_homework()
+        {
+            return View();
+        }
+
+        public IActionResult homeworkhub_submission()
+        {
+            return View();
+        }
+
+    }
+}
