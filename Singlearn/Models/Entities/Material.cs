3bc633f3c38c347df40af1ee3936ce065d4ed79b
wigsicle <wigsicle@gmail.com>
2024-07-06 16:31:44 +0800

    material model

--- Singlearn/Models/Entities/Material.cs
+++ Singlearn/Models/Entities/Material.cs
@@ -1,6 +1,28 @@
-﻿namespace Singlearn.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Singlearn.Models.Entities
 {
     public class Material
     {
+        [Key]
+        public int material_id { get; set; }
+
+        public int subject_id { get; set; }
+
+        public string teacher_id { get; set; }
+
+        public int class_id { get; set; }
+
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+        public int chapter_id { get; set; }
+        public string type { get; set; }
+
+        public string link { get; set; }
+
+        public string status { get; set; }
+
     }
 }
