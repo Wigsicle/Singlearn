86784f8645755b8273a804170d10fdb9b071a823
wigsicle <wigsicle@gmail.com>
2024-07-06 16:41:12 +0800

    studentteacherclass model

--- Singlearn/Models/Entities/StudentTeacherClass.cs
+++ Singlearn/Models/Entities/StudentTeacherClass.cs
@@ -1,6 +1,16 @@
-﻿namespace Singlearn.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Singlearn.Models.Entities
 {
     public class StudentTeacherClass
     {
+        [Key]
+        public int stc_id { get; set; }
+
+        public int subject_id { get; set; }
+
+        public string teacher_id { get; set; }
+
+        public int class_id { get; set; }
     }
 }
