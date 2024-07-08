c84d81390ceda681625873342466c7524cfaf551
Joel <161714040+drjoelkoh@users.noreply.github.com>
2024-07-08 20:40:24 +0800

    Update SubjectController.cs

--- Singlearn/Controllers/SubjectController.cs
+++ Singlearn/Controllers/SubjectController.cs
@@ -1,83 +1,157 @@
-﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Singlearn.Data;
+using Singlearn.Models.Entities;
 
 namespace Singlearn.Controllers
 {
-    public class SubjectController : Controller
+    public class SubjectsController : Controller
     {
-        // GET: SubjectController
-        public ActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public SubjectsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Subjects
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Subjects.ToListAsync());
+        }
+
+        // GET: Subjects/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects
+                .FirstOrDefaultAsync(m => m.subject_id == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // GET: Subjects/Create
+        public IActionResult Create()
         {
             return View();
         }
 
-        // GET: SubjectController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: SubjectController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: SubjectController/Create
+        // POST: Subjects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create([Bind("subject_id,name,year,academic_level,no_chapters,image")] Subject subject)
         {
-            try
+            if (ModelState.IsValid)
             {
+                _context.Add(subject);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View(subject);
         }
 
-        // GET: SubjectController/Edit/5
-        public ActionResult Edit(int id)
+        // GET: Subjects/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            return View(subject);
         }
 
-        // POST: SubjectController/Edit/5
+        // POST: Subjects/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, [Bind("subject_id,name,year,academic_level,no_chapters,image")] Subject subject)
         {
-            try
+            if (id != subject.subject_id)
             {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subject);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubjectExists(subject.subject_id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View(subject);
         }
 
-        // GET: SubjectController/Delete/5
-        public ActionResult Delete(int id)
+        // GET: Subjects/Delete/5
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects
+                .FirstOrDefaultAsync(m => m.subject_id == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
         }
 
-        // POST: SubjectController/Delete/5
-        [HttpPost]
+        // POST: Subjects/Delete/5
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            try
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject != null)
             {
-                return RedirectToAction(nameof(Index));
+                _context.Subjects.Remove(subject);
             }
-            catch
-            {
-                return View();
-            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubjectExists(int id)
+        {
+            return _context.Subjects.Any(e => e.subject_id == id);
         }
     }
 }
