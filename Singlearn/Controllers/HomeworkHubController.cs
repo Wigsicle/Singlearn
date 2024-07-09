286bb21185ed0012320f4a2ef94ce6af23a07d34
linfvng <linfxng@gmail.com>
2024-07-09 16:44:22 +0800

    added homework html

---
+++ Singlearn/Controllers/HomeworkHubController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SinglearnWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SinglearnWeb.Controllers
+{
+    public class HomeworkHubController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public HomeworkHubController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult staff_subject()
+        {
+            return View();
+        }
+
+        public IActionResult staff_class()
+        {
+            return View();
+        }
+
+        public IActionResult staff_homework()
+        {
+            return View();
+        }
+
+        public IActionResult staff_submission()
+        {
+            return View();
+        }
+
+    }
+}
