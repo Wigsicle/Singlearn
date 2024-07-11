438ab796c00c1b0debd33c0e60e71d3504dc1f11
Wigsicle <Wigsicle@gmail.com>
2024-07-11 12:57:50 +0800

    Added file_type attribute to material

---
+++ Singlearn/Migrations/20240711045449_AddFileTypeToMaterial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Singlearn.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFileTypeToMaterial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "file_type",
+                table: "Materials",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
