14ddcf3a8cae03a52a148a9c02393e7710c1c48f
Wigsicle <Wigsicle@gmail.com>
2024-07-11 12:58:25 +0800

    Added file_type to material model

--- Singlearn/Models/Entities/Material.cs
+++ Singlearn/Models/Entities/Material.cs
@@ -16,5 +16,7 @@ namespace Singlearn.Models.Entities
         public string link { get; set; }
         public string status { get; set; }
         public byte[] data { get; set; } // For storing varbinary(max) in the database
+
+        public string file_type { get; set; }
     }
 }
