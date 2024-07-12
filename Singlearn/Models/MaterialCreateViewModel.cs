48eb9581e892f04913ce5376c5345eaaa1380111
wigsicle <wigsicle@gmail.com>
2024-07-12 14:52:30 +0800

    Same as the one below

--- Singlearn/Models/MaterialCreateViewModel.cs
+++ Singlearn/Models/MaterialCreateViewModel.cs
@@ -6,14 +6,14 @@ namespace Singlearn.ViewModels
     {
         public int subject_id { get; set; }
         public string teacher_id { get; set; }
-        public string class_id { get; set; }
+        public string? class_id { get; set; }
         public string name { get; set; }
         public string description { get; set; }
         public int chapter_id { get; set; }
         public string type { get; set; }
-        public string link { get; set; }
+        public string? link { get; set; }
         public string status { get; set; }
-        public IFormFile DataFile { get; set; } // For file upload
+        public IFormFile? DataFile { get; set; } // For file upload
         public string file_type { get; set; }
         public IFormFile PDFFile { get; set; }
     }
