505ea99e03ef5e75b68cbb87aae2c61044712e8d
Khoo Jun Ye <57204255+KJY-99@users.noreply.github.com>
2024-07-14 19:44:15 +0800

    changes for materials
    visibility of input depending on the type the teacher chooses (video lesson, etc.);
    added migration for changing various things to nullable in Materials Table.
    Added drop down for type and status.
    Some UI fixes for chaptermain too

---
+++ Singlearn/Migrations/20240714110746_NullableMaterials3.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Singlearn.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableMaterials3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+              name: "file_type",
+              table: "Materials",
+              type: "nvarchar(max)",
+              nullable: true,
+              oldClrType: typeof(string),
+              oldType: "nvarchar(max)",
+              oldNullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+              name: "pdf_file",
+              table: "Materials",
+              type: "varbinary(max)",
+              nullable: true,
+              oldClrType: typeof(string),
+              oldType: "varbinary(max)",
+              oldNullable: false);
+
+        }
+    }
+}
--- Singlearn/Models/MaterialEditViewModel.cs
+++ Singlearn/Models/MaterialEditViewModel.cs
@@ -9,14 +9,14 @@ namespace Singlearn.ViewModels
         public string teacher_id { get; set; }
         public string? class_id { get; set; }
         public string name { get; set; }
-        public string description { get; set; }
+        public string? description { get; set; }
         public int chapter_id { get; set; }
         public string type { get; set; }
         public string? link { get; set; }
         public string status { get; set; }
         public IFormFile? DataFile { get; set; } // For file upload
-        public string file_type { get; set; }
-        public IFormFile PDFFile { get; set; }
+        public string? file_type { get; set; }
+        public IFormFile? PDFFile { get; set; }
     }
 }
 
