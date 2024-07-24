f129863c02df80c0764b37b443a81378cf944ec2
w4nying <158993035+w4nying@users.noreply.github.com>
2024-07-24 20:59:24 +0800

    new ViewModel files for Profile

---
+++ Singlearn/Models/StaffProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Singlearn.ViewModels
+{
+    public class StaffProfileViewModel
+    {
+        public string staff_id { get; set; }
+        public string name { get; set; }
+        public string contact_no { get; set; }
+        public List<string> classes { get; set; }
+    }
+}
---
+++ Singlearn/Models/StudentProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Singlearn.ViewModels
+{
+    public class StudentProfileViewModel
+    {
+        public string student_id { get; set; }
+        public string name { get; set; }
+        public string contact_no { get; set; }
+        public string class_id { get; set; }
+    }
+}
